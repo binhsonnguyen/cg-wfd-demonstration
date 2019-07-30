79f6a2dc5e407dd9d70700e970fb62a2e212d1ce
edward <son@binhsonnguyen.com>
2019-07-30 13:56:56 +0700

    Import browser module for root module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent
+  ],
+  imports: [
+    BrowserModule
   ]
 })
 export class AppModule {
