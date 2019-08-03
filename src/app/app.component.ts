6179fe2cc9b833e5e12d4ec71c540c399dd65790
edward <son@binhsonnguyen.com>
2019-08-03 10:32:23 +0700

    Use ngModel directive from FormModule to implement update function, instead of manual.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,4 @@ export class AppComponent {
     title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
     url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
   };
-
-  updatePost() {
-    this.post.title = document.getElementById('post-title').value;
-    this.post.url = document.getElementById('post-url').value;
-  }
 }
