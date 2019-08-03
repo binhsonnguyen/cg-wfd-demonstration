7179edf4c0c3ec66d0ea6e4750e5a3c67b107bfb
edward <son@binhsonnguyen.com>
2019-08-03 10:23:31 +0700

    Create update post method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,7 @@ export class AppComponent {
     title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
     url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
   };
+
+  updatePost() {
+  }
 }
