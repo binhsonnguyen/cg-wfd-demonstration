b3e15ca3a5d40242cc490cf06904664a03ae7632
edward <son@binhsonnguyen.com>
2019-08-06 23:39:37 +0700

    Remove shared component from root module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,10 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {FooterComponent} from './shared/footer/footer.component';
-import {HeaderComponent} from './shared/header/header.component';
 import {PostsListComponent} from './posts/posts-list/posts-list.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faKiss} from '@fortawesome/free-regular-svg-icons';
-import {KissletComponent} from './shared/kisslet/kisslet.component';
 import {PostsHomeComponent} from './posts/posts-home/posts-home.component';
 import {PostsContributeComponent} from './posts/posts-contribute/posts-contribute.component';
 import {PostCommentsComponent} from './posts/post-comments/post-comments.component';
@@ -17,10 +14,7 @@ import {AppRoutingModule} from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
     PostsListComponent,
-    KissletComponent,
     PostsHomeComponent,
     PostsContributeComponent,
     PostCommentsComponent
