ae8b50516a141c0ae614deda39b93c7b95a08450
edward <son@binhsonnguyen.com>
2019-08-06 23:33:05 +0700

    Extract routes to const.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,31 @@
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {PostsHomeComponent} from './posts/posts-home/posts-home.component';
 import {PostCommentsComponent} from './posts/post-comments/post-comments.component';
 import {PostsContributeComponent} from './posts/posts-contribute/posts-contribute.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PostsHomeComponent
+  },
+  {
+    path: 'news',
+    component: PostsHomeComponent
+  },
+  {
+    path: 'news/:id',
+    component: PostCommentsComponent
+  },
+  {
+    path: 'contrib',
+    component: PostsContributeComponent
+  }
+];
+
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {
-        path: '',
-        component: PostsHomeComponent
-      },
-      {
-        path: 'news',
-        component: PostsHomeComponent
-      },
-      {
-        path: 'news/:id',
-        component: PostCommentsComponent
-      },
-      {
-        path: 'contrib',
-        component: PostsContributeComponent
-      }
-    ])
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
