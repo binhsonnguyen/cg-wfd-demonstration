a726857de1f9072187c10a328bd9a83734ffe359
edward <son@binhsonnguyen.com>
2019-08-06 21:59:23 +0700

    Fetch post by id that get from route's param map.

--- src/app/posts/post-comments/post-comments.component.ts
+++ src/app/posts/post-comments/post-comments.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Post} from '../../core/Post';
 import {PostService} from '../../post.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-post-comments',
@@ -9,12 +10,12 @@ import {PostService} from '../../post.service';
 export class PostCommentsComponent implements OnInit {
   post: Post;
 
-  constructor(private postService: PostService) {
+  constructor(private postService: PostService, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    const id = 0;
-    this.post = this.postService.fetch(id);
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.post = this.postService.fetch(+id);
   }
 
 }
