8d710ee0c5da3d87928d2a707636c3dedaabe270
edward <son@binhsonnguyen.com>
2019-08-06 21:57:41 +0700

    Use post service to fetch a dummy post for post detail screen.

--- src/app/posts/post-comments/post-comments.component.ts
+++ src/app/posts/post-comments/post-comments.component.ts
@@ -1,8 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Post} from '../../core/Post';
+import {PostService} from '../../post.service';
 
 @Component({
   selector: 'app-post-comments',
   templateUrl: './post-comments.component.html'
 })
-export class PostCommentsComponent {
+export class PostCommentsComponent implements OnInit {
+  post: Post;
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit(): void {
+    const id = 0;
+    this.post = this.postService.fetch(id);
+  }
+
 }
