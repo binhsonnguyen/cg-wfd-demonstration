c8e64b4e742af1ae480cff84e23db8251d2c9085
edward <son@binhsonnguyen.com>
2019-08-08 13:54:17 +0700

    Inject http client dependence.

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,63 +1,16 @@
 import {Post} from './core/Post';
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   private autoIncreasementId = 0;
-  private posts: Post[] = [
-    {
-      id: this.autoIncreasementId++,
-      title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
-      url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40',
-      kissed: 1920,
-      comments: [
-        {
-          id: this.autoIncreasementId++,
-          content: 'Good to consulting.',
-          author: 'An Duong Vuong',
-          kissed: 17
-        },
-        {
-          id: this.autoIncreasementId++,
-          content: 'So how the evolution of multi-shot bow?',
-          author: 'My Chau',
-          kissed: 11
-        },
-        {
-          id: this.autoIncreasementId++,
-          content: 'Greate question!',
-          author: 'Trong Thuy',
-          kissed: 3
-        },
-      ]
-    },
-    {
-      id: this.autoIncreasementId++,
-      title: 'Game of Life',
-      url: 'https://thefullsnack.com/posts/game-of-life.html',
-      kissed: 1280
-    },
-    {
-      id: this.autoIncreasementId++,
-      title: 'Nguyên tắc thiết kế REST API',
-      url: 'https://medium.com/eway/nguyên-tắc-thiết-kế-rest-api-23add16968d7',
-      kissed: 1024
-    },
-    {
-      id: this.autoIncreasementId++,
-      title: 'Why You Only Need to Test with 5 Users',
-      url: 'https://www.nngroup.com/articles/why-you-only-need-to-test-with-5-users/',
-      kissed: 800
-    },
-    {
-      id: this.autoIncreasementId++,
-      title: 'Let’s Build A Web Server. Part 1.',
-      url: 'https://ruslanspivak.com/lsbaws-part1/',
-      kissed: 640
-    }
-  ];
+  private posts: Post[];
+
+  constructor(private http: HttpClient) {
+  }
 
   fetchAll(): Post[] {
     return this.posts;
