9d9fdbcd9f4ce85fee052d2815c10eadd44a0aca
edward <son@binhsonnguyen.com>
2019-08-10 23:24:36 +0700

    Declare that AppComponent is a component of AppModule, and is bootstrap component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
 
-@NgModule()
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent]
+})
 export class AppModule {
 }
