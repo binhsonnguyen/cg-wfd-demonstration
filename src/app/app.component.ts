79fff86ec7ff06f823d349fcc6b5b34eb28d3f8b
edward <son@binhsonnguyen.com>
2019-08-11 10:42:04 +0700

    Use FormsModule's ngModel directive to update article instead of manual.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component, Inject} from '@angular/core';
-import {DOCUMENT} from '@angular/common';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +9,4 @@ export class AppComponent {
     title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
     url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
   };
-
-  constructor(@Inject(DOCUMENT) private document) {
-  }
-
-  updateArticle() {
-    this.article.title = this.document.getElementById('article-title').value;
-    this.article.url = this.document.getElementById('article-url').value;
-  }
 }
