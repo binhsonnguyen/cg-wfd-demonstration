69e1954245548ef17055e4ebeab2175719c0152c
edward <son@binhsonnguyen.com>
2019-08-12 11:41:52 +0700

    Transform update article to a form to add new article.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,11 @@ import {ARTICLES} from './ARTICLES';
 export class AppComponent {
   articles = ARTICLES;
 
-  article = {
-    title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
-    url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
+  editingArticle = {
+    title: '',
+    url: ''
   };
+
+  addArticle() {
+  }
 }
